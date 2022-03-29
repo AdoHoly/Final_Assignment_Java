d399fde5213e91f6bfdde12c5219cfd45d575763
AdoHoly <98894398+AdoHoly@users.noreply.github.com>
2022-03-29 12:32:27 +0200

    1. added article delete funcionality + whole project

---
+++ src/main/java/sk/ness/academy/dao/ArticleDAO.java
@@ -0,0 +1,21 @@
+package sk.ness.academy.dao;
+
+import java.util.List;
+
+import sk.ness.academy.domain.Article;
+
+public interface ArticleDAO {
+
+	  /** Returns {@link Article} with provided ID */
+	  Article findByID(Integer articleId);
+
+	  /** Returns all available {@link Article}s */
+	  List<Article> findAll();
+
+	  /** Persists {@link Article} into the DB */
+	  void persist(Article article);
+
+	  /** Deletes {@link Article} with provided ID */
+	  void deleteByID(Integer articleId);
+
+	}
---
+++ src/main/java/sk/ness/academy/dao/ArticleHibernateDAO.java
@@ -0,0 +1,39 @@
+package sk.ness.academy.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import sk.ness.academy.domain.Article;
+
+@Repository
+public class ArticleHibernateDAO implements ArticleDAO {
+
+  @Resource(name = "sessionFactory")
+  private SessionFactory sessionFactory;
+
+  @Override
+  public Article findByID(final Integer articleId) {
+    return (Article) this.sessionFactory.getCurrentSession().get(Article.class, articleId);
+  }
+
+  @Override
+  public void deleteByID(final Integer articleId){
+     this.sessionFactory.getCurrentSession().delete(findByID(articleId));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<Article> findAll() {
+    return this.sessionFactory.getCurrentSession().createSQLQuery("select * from articles").addEntity(Article.class).list();
+  }
+
+  @Override
+  public void persist(final Article article) {
+    this.sessionFactory.getCurrentSession().saveOrUpdate(article);
+  }
+
+}
